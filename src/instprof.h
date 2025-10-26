14061bfe5d6c134d83c9e50bf74210d82534d7a9
J-Cowsert <cowsert.jsec@proton.me>
2025-10-26 17:21:22 -0500

    Cleanup API

--- src/instprof.h
+++ src/instprof.h
@@ -1,21 +1,20 @@
 #pragma once
 
-// Will other macros be used as inputs?  
-#define IP_CONCAT_INNER(a, b) a##b 
-#define IP_CONCAT(a, b) IP_CONCAT_INNER(a, b)
-
 // Temp toggle
 #ifndef IP_ENABLE
     #define IP_ENABLE 1
 #endif
+
+#define IP_CONCAT_INNER(a, b) a##b 
+#define IP_CONCAT(a, b) IP_CONCAT_INNER(a, b)
 
 
 #if IP_ENABLE
 
     #include "core/Zone.h"
    
-    #define IP_NAMED_SCOPE(name_literal) instprof::ZoneScope IP_CONCAT(_ipZone_, __COUNTER__){ instprof::make_callsite(name_literal) }
-    #define IP_FUNC_SCOPE() instprof::ZoneScope IP_CONCAT(_ipZone_, __COUNTER__){ instprof::make_callsite(__func__) }
+    #define IP_NAMED_SCOPE(name_literal) static constexpr instprof::CallsiteInfo IP_CONCAT(_ipCs_, __LINE__) = instprof::MakeCallsite((name_literal)); instprof::ZoneScope IP_CONCAT(_ipZone_, __COUNTER__){ &IP_CONCAT(_ipCs_, __LINE__) }
+    #define IP_FUNC_SCOPE() static constexpr instprof::CallsiteInfo IP_CONCAT(_ipCs_, __LINE__) = instprof::MakeCallsite(__func__); instprof::ZoneScope IP_CONCAT(_ipZone_, __COUNTER__){ &IP_CONCAT(_ipCs_, __LINE__) }
 
 #else 
 
